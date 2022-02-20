7a4fcf8a5eff1f1d259ab69eaad9ea0646ddf42f
samstack54 <39880906+samstack54@users.noreply.github.com>
2022-02-20 19:35:22 +0900

    Add files via upload

---
+++ Blink.ino
@@ -0,0 +1,11 @@
+
+ void setup() {
+  pinMode(25, OUTPUT);
+ }
+
+ void loop() {
+  digitalWrite(25, HIGH);   
+  delay(1000);                       
+  digitalWrite(25, LOW);    
+  delay(1000); 
+ }                      
